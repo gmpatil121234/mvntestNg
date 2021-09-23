a44923a54e18eccd2b9abc42e781b0bf26dc0e73
Ganesh Patil <gmpatil121234@gmail.com>
2021-09-23 19:51:21 +0530

    my first gidhub project commit

---
+++ src/test/java/simpalgithubjava/gitHubClass.java
@@ -0,0 +1,22 @@
+package simpalgithubjava;
+
+public class gitHubClass {
+	public int a=5;
+	public int b=8;
+	public int c=a+b;
+	
+		public void addMethod()
+		{
+			System.out.println(c);
+		
+	}
+
+	public static void main(String[] args) {
+		
+		gitHubClass obj = new gitHubClass();
+		obj.addMethod();
+		
+		
+	}
+
+}
